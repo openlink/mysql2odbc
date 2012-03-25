8d6da0ec4b31e582ee8719610549578b8ccd87cc
Patrick van Kleef <pkleef@openlinksw.com>
2012-03-25 01:38:25 +0100

    Updated copyright year to 2012

--- mtest.c
+++ mtest.c
@@ -7,7 +7,7 @@
  *
  *  mysql2odbc - A MySQL to ODBC bridge library
  *  
- *  Copyright (C) 2003 by OpenLink Software <iodbc@openlinksw.com>
+ *  Copyright (C) 2003-2012 OpenLink Software <iodbc@openlinksw.com>
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Library General Public
